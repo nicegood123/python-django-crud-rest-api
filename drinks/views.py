e585aa3367b4d13cf6ea0f0c3fd89be934c748bf
Gisan Geff <ggranraniego@gmail.com>
2022-09-27 13:08:28 +0800

    Add add drink function

--- drinks/views.py
+++ drinks/views.py
@@ -1,14 +1,24 @@
+import imp
 from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from drinks import serializers
 
 
+@api_view(['GET', 'POST'])
 def drink_list(request):
 
-    # get all the drinks
-    # serialize them
-    # return json
+    if request.method == 'GET':
+        drinks = Drink.objects.all()
+        serializer = DrinkSerializer(drinks, many=True)
+        return JsonResponse({'drinks': serializer.data})
 
-    drinks = Drink.objects.all()
-    serializer = DrinkSerializer(drinks, many=True)
-    return JsonResponse({'drinks': serializer.data}, safe=False)
+    if request.method == 'POST':
+        serializer = DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
