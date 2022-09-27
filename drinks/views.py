436c42912c718f88dcb37c8ea974e1fc0d9361be
Gisan Geff <ggranraniego@gmail.com>
2022-09-27 13:16:45 +0800

    Add view detail drink api

--- drinks/views.py
+++ drinks/views.py
@@ -1,4 +1,5 @@
 import imp
+import re
 from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
@@ -22,3 +23,20 @@ def drink_list(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def drink_detail(request, id):
+
+    try:
+        drink = Drink.objects.get(pk=id)
+    except Drink.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = DrinkSerializer(drink)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        pass
+    elif request.method == 'DELETE':
+        pass
